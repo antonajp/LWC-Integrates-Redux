6a26fc126b53d1632b63791e86c400e80c4884f6
Sky Chen <sky.chen@salesforce.com>
2022-04-22 14:04:46 -0700

    add outstanding todos component

--- force-app/main/default/lwc/store/selectors.js
+++ force-app/main/default/lwc/store/selectors.js
@@ -3,3 +3,7 @@ import { reselect } from 'c/reduxLibs';
 const { createSelector } = reselect;
 
 export const todoSelector = createSelector(state => state.todoReducer, todoReducer => todoReducer.todos || []);
+
+export const todoBadgeSelector = createSelector(todoSelector, (todos) => {
+    return todos.filter((todo) => !todo.completed);
+});
---
+++ force-app/main/default/lwc/todoBadge/todoBadge.js
@@ -0,0 +1,7 @@
+import { LightningElement, track } from 'lwc';
+import { selectors, actionCreators } from 'c/store';
+import { lwcReduxMixin } from 'c/lwcBindings';
+const { todoBadgeSelector } = selectors;
+export default class TodoBadge extends lwcReduxMixin({ mapStateToProps: todoBadgeSelector, propName: 'outstandingTodos' }, LightningElement) {
+    outstandingTodos = [];
+}
