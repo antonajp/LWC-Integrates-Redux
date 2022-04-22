9b347676382240d10c8614699c000fa1e1beaa54
Sky Chen <sky.chen@salesforce.com>
2022-04-22 16:22:58 -0700

    minor enhancement

--- force-app/main/default/lwc/todoBadge/todoBadge.js
+++ force-app/main/default/lwc/todoBadge/todoBadge.js
@@ -4,4 +4,8 @@ import { lwcReduxMixin } from 'c/lwcBindings';
 const { todoBadgeSelector } = selectors;
 export default class TodoBadge extends lwcReduxMixin({ mapStateToProps: todoBadgeSelector, propName: 'outstandingTodos' }, LightningElement) {
     outstandingTodos = [];
+
+    get hasTodos() {
+        return this.outstandingTodos && this.outstandingTodos.length;
+    }
 }
