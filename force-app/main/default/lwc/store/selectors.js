e67faf4fe8171407f1b6327919e9be56964ac5f6
Sky Chen <sky.chen@salesforce.com>
2022-04-22 17:02:02 -0700

    add progress ring

--- force-app/main/default/lwc/store/selectors.js
+++ force-app/main/default/lwc/store/selectors.js
@@ -5,5 +5,13 @@ const { createSelector } = reselect;
 export const todoSelector = createSelector(state => state.todoReducer, todoReducer => todoReducer.todos || []);
 
 export const todoBadgeSelector = createSelector(todoSelector, (todos) => {
-    return todos.filter((todo) => !todo.completed);
+    const outstandingTodos = todos.filter((todo) => !todo.completed);
+    const progress = todos && todos.length
+        ? (todos.length - outstandingTodos.length)/todos.length * 100
+        : 100;
+
+    return {
+        outstandingTodos,
+        progress
+    }
 });
--- force-app/main/default/lwc/todoBadge/todoBadge.js
+++ force-app/main/default/lwc/todoBadge/todoBadge.js
@@ -1,11 +1,13 @@
-import { LightningElement, track } from 'lwc';
-import { selectors, actionCreators } from 'c/store';
+import { LightningElement } from 'lwc';
+import { selectors } from 'c/store';
 import { lwcReduxMixin } from 'c/lwcBindings';
 const { todoBadgeSelector } = selectors;
-export default class TodoBadge extends lwcReduxMixin({ mapStateToProps: todoBadgeSelector, propName: 'outstandingTodos' }, LightningElement) {
+export default class TodoBadge extends lwcReduxMixin({ mapStateToProps: todoBadgeSelector,  }, LightningElement) {
     outstandingTodos = [];
+    progress = 0;
 
     get hasTodos() {
         return this.outstandingTodos && this.outstandingTodos.length;
     }
+
 }
